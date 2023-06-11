ef01b0d2b35fa07d0dbdfbbf06c02c18203d3335
sathyanarayanan <vijaysathi2001@gmail.com>
2023-06-11 12:36:59 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() {//this function will get called from index.js
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Card1 /> 
+      <Card2 />
+      <Card3 /> 
     </div>
   );
 }
 
-export default App;
+// first card
+function Card1(){
+  return(
+    <div className="card">
+      <div className="card_body">
+        
+        <div className="card_header">
+            <h5 className="card_title">FREE</h5>
+            <h2 className="card_price">$0<span class="period">/month</span></h2>
+        </div>
+        <hr></hr>
+       
+        <div className="card_description">
+        <ul>
+              <li className="list_checked">Single User</li>
+              <li className="list_checked">5GB Storage</li>
+              <li className="list_checked">Unlimited Public Projects</li>
+              <li className="list_checked">Community Access</li>
+              <li className="text-muted">Unlimited Private Projects</li>
+              <li className="text-muted">Dedicated Phone Support</li>
+              <li className="text-muted">Free Subdomain</li>
+              <li className="text-muted">Monthly Status Reports</li>
+            </ul>
+        </div>
+        
+        <div className="card_footer">
+             <button className="card_btn">BUTTON</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+function Card2(){
+  return(
+    <div className="card">
+      <div className="card_body">
+      
+      <div className="card_header">
+            <h5 className="card_title">PLUS</h5>
+            <h2 className="card_price">$9<span class="period">/month</span></h2>
+        </div>
+        <hr></hr>
+        
+        <div className="card_description">
+        <ul>
+              <li className="list_checked"><b>5 Users</b></li>
+              <li className="list_checked">50GB Storage</li>
+              <li className="list_checked">Unlimited Public Projects</li>
+              <li className="list_checked">Community Access</li>
+              <li className="list_checked">Unlimited Private Projects</li>
+              <li className="list_checked">Dedicated Phone Support</li>
+              <li className="list_checked">Free Subdomain</li>
+              <li className="text-muted">Monthly Status Reports</li>
+            </ul>
+        </div>
+        
+        <div className="card_footer">
+             <button className="card_btn">BUTTON</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+// third card
+function Card3(){
+  return(
+    <div className="card">
+      <div className="card_body">
+        
+      <div className="card_header">
+            <h5 className="card_title">PRO</h5>
+            <h2 className="card_price">$49<span class="period">/month</span></h2>
+        </div>
+        <hr></hr>
+        
+        <div className="card_description">
+        <ul>
+              <li className="list_checked">Unlimited User</li>
+              <li className="list_checked">150GB Storage</li>
+              <li className="list_checked">Unlimited Public Projects</li>
+              <li className="list_checked">Community Access</li>
+              <li className="list_checked">Unlimited Private Projects</li>
+              <li className="list_checked">Dedicated Phone Support</li>
+              <li className="list_checked"><b>Unlimited </b>Free Subdomains</li>
+              <li className="list_checked">Monthly Status Reports</li>
+            </ul>
+        </div>
+        
+        <div className="card_footer">
+             <button className="card_btn">BUTTON</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
